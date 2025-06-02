d8706a7ef81dd9b55463b2c2fa5ebf3b1112eacf
Micael Andrade <mikael.java@gmail.com>
2025-06-02 20:11:03 -0300

    feature: O(1) pilhas e arrays

--- java/AlgotimosConstantes.java
+++ java/AlgotimosConstantes.java
@@ -1,10 +1,15 @@
+import java.util.Stack;
+
 public class AlgotimosConstantes {
-    static final int N_VEZES = 10000;
+    static final int N_VEZES = 100;
 
     public static void main(String[] args) {
 
         AlgotimosConstantes.testarParNVezes(N_VEZES);
         AlgotimosConstantes.trocarValorNVezes(N_VEZES);
+        AlgotimosConstantes.acessarElementoArrayNvezes(N_VEZES);
+        AlgotimosConstantes.inserirRemovePilha(N_VEZES);
+
 
     }
 
@@ -37,6 +42,28 @@ public class AlgotimosConstantes {
             trocarValorXY(j, i);
             System.out.println("Trocou!");
         }
+    }
+
+    private static void acessarElementoArray(int index, int[] arr) {
+        System.out.println("Index acessado: " + arr[index]);
+    }
+
+    private static void acessarElementoArrayNvezes(int N) {
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            int index = i % N;
+            acessarElementoArray(index, arr); // módulo para acessar posições diferentes do array
+        }
+    }
+
+    private static void inserirRemovePilha(int N) {
+        Stack<String> minhaPilha = new Stack<>();
+        for (int i = 0; i < N; i++) {
+            minhaPilha.push("Item: " + i);
+            System.out.print(i + " adicionado");
+            System.out.println("Item removido:" + minhaPilha.pop());
+        }
+
     }
 
 }
