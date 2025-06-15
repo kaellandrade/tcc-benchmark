a1879f44170fcf6670e3510870c42ac4c1f5bba5
Micael Andrade <mikael.java@gmail.com>
2025-06-14 21:44:20 -0300

    feature: implementando de exponenciacao por quadrados

--- java/AlgoritmosLogN.java
+++ java/AlgoritmosLogN.java
@@ -4,13 +4,16 @@ public class AlgoritmosLogN {
     static final int LENGTH_INSERT_HEAPIFY = 10;
     static final int ARRAY_LENGTH = 100000;
     static final int[] ARRAY_ORDENADO = new int[ARRAY_LENGTH];
-    static final int BITWISE_NUMBER= 342343; // em binário é 1010011100101000111
+    static final int BITWISE_NUMBER = 342343; // em binário é 1010011100101000111
 
     public static void main(String[] args) {
-        AlgoritmosLogN.testBuscaBinaria();
+        // AlgoritmosLogN.testBuscaBinaria();
         // AlgoritmosLogN.testHeapify();
-        int totalBits = AlgoritmosLogN.countSetBits(BITWISE_NUMBER);
-        System.out.println(totalBits);
+        // int totalBits = AlgoritmosLogN.countSetBits(BITWISE_NUMBER);
+        // System.out.println(totalBits);
+
+        long resultadoBaseExp = AlgoritmosLogN.exponentiatingBysquaring(3,10);
+        System.out.println(resultadoBaseExp);;
     }
 
     private static void testHeapify() {
@@ -64,12 +67,14 @@ public class AlgoritmosLogN {
     }
 
     /**
-     * Pega um número inteiro e retorna todos os bits ativos em sua representação binária.
+     * Pega um número inteiro e retorna todos os bits ativos em sua representação
+     * binária.
      * 
-     * Time Complexity: O(log n) 
+     * Time Complexity: O(log n)
      * Auxiliary Space: O(1)
      * 
      * Referências: https://www.geeksforgeeks.org/dsa/count-set-bits-in-an-integer/
+     * 
      * @param n
      * @return
      */
@@ -81,6 +86,29 @@ public class AlgoritmosLogN {
         }
 
         return totalBits1;
+    }
+
+   public static long exponentiatingBysquaring(long base, long expoente) {
+        if (expoente < 0) {
+            throw new IllegalArgumentException("Expoente não pode ser negativo.");
+        }
+        
+        long resultado = 1L; // Inicializa o resultado como 1 (L indica que é um long)
+
+        while (expoente > 0) {
+            // Se o expoente for ímpar (o último bit é 1)
+            if (expoente % 2 == 1) {
+                resultado *= base;
+            }
+
+            // Eleva a base ao quadrado para a próxima iteração
+            base *= base;
+
+            // Divide o expoente por 2 (deslocamento de bits para a direita)
+            expoente /= 2;
+        }
+
+        return resultado;
     }
 
 }
