65737dc078a7850aebcc62c917a24540cc498452
Thiago Freire de Carvalho <thiago.freire45@gmail.com>
2026-01-17 17:29:12 -0300

    feat: add shadcn/ui

--- web-app/vite.config.ts
+++ web-app/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,5 +6,10 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),VitePWA()],
+  plugins: [react(), tailwindcss(), VitePWA()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
