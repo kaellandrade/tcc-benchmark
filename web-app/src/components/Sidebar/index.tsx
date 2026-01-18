8a93f0be01731e9170e2c726d035be917ce5b7d9
Micael Andrade <mikael.java@gmail.com>
2026-01-18 13:20:50 -0300

    style: ajustando gradiente da sidebar no modo escuro

--- web-app/src/components/Sidebar/index.tsx
+++ web-app/src/components/Sidebar/index.tsx
@@ -111,7 +111,7 @@ export function Sidebar({
           <div
               className={`flex-1 flex flex-col transition-all duration-300 ${
                   isDarkMode
-                      ? "bg-gradient-to-b from-secondary/20 to-background" 
+                      ? "bg-gradient-to-b from-secondary/80 to-background" 
                       : "bg-gradient-to-b from-secondary/30 to-background" 
               }`}
           >
